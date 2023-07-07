5df89a315a724f6d813b0c16d4de1a6c2f2475d0
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2023-07-07 11:03:26 +0300

    Add cors, express-json, and express-encoded middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import cors from "cors";
 import { productRouter } from "./routes/product.routes";
 import { updateRouter } from "./routes/update.routes";
 import { updatePointRouter } from "./routes/update-point.routes";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", productRouter);
 app.use("/api", updateRouter);
